52f303a2fdec98292881bceb8582fde93b1e9979
JuliaDav <martyukhinayulia@yandex.ru>
2019-02-15 19:31:53 +0300

    +

--- Lesson_8/hw08_easy.py
+++ Lesson_8/hw08_easy.py
@@ -4,9 +4,16 @@
 создается корректно - создать экземпляр класса и обратиться к значению
 элемента словаря, как к атрибуту класса.
 '''
+class WorkerBuilder:
+    def __init__(self, name, information):
+        for key, value in name.items():
+            setattr(self, key, value)
+        for key, value in information.items():
+            setattr(self, key, value)
 
-
-
+worker = WorkerBuilder({"name": "Петр","surname": "Алексеев"}, {"age": 10, 'income': 20000})
+print(f'{worker.name} {worker.surname}')
+print(f'Возраст: {worker.age}, доход: {worker.income}')
 
 '''
 Задание 2.
@@ -16,5 +23,15 @@
 Внутри класса должно быть предусмотрено делегирование этой функции методу (clear)
 списка.
 '''
+class Wrapper:
+    def __init__(self, object):
+        self.wrapped = object
+        print(self.wrapped)
 
+    def __getattr__(self, attrname):
+        return getattr(self.wrapped, attrname)
+
+x = Wrapper([1, 2, 3])
+x.clear()
+print(x.wrapped)
 
